27e30766d876a35a1e230206bd2f6f4278386cbb
matheus <matheus.ferraro@gmail.com>
2023-05-14 16:32:57 -0300

    content created new page

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,34 @@
+export const dataFake = [
+ {
+    "id": "1",
+    "title": "CAPIVARAS ASSASINAS",
+    "description": "Capivaras aterrorisam a cidade de ipaussu, atacam moradores e os comem vivos, com seus dentes trituram suas cabeças e comem seus cerebros roubando seus conhecimentos",
+    "photoCover": "https://static.biologianet.com/2023/01/capivara.jpg"
+
+ },
+ {
+    "id": "2",
+    "title": "Ipaussu Limpa o Lago",
+    "description": "Apos 10 anos ipaussu decidi limpar o lago e retirar os carrapatos da cidade",
+    "photoCover": "https://ipaussu.sp.gov.br/galerias/fotos/64/5.jpg"
+
+ },
+
+ {
+   "id": "3",
+   "title": "Camping de Ipaussu é Reformado",
+   "description": "Apos um investimento, o camping de Ipaussu foi reformada aumentando a segurança e conservando a natureza",
+   "photoCover": "https://ipaussu.sp.gov.br/galerias/fotos/66/8.jpg"
+
+},
+
+
+{
+   "id": "4",
+   "title": "Ipaussu de cima",
+   "description": "Foto tirada de um drone pegando um campo de visao de cima da cidade de Ipaussu",
+   "photoCover": "https://3.bp.blogspot.com/-b4GCK2UbOW4/VbqYrf7xowI/AAAAAAAACBI/8LO_3l9Gq-g/s1600/Ipaussu%252C%2Bcidade%2Bjoia.jpg"
+
+}
+ 
+]
